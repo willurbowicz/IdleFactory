d8ebd0cc3d3f98797377599c1c626aa5558c5277
Will Urbowicz <122821261+willurbowicz@users.noreply.github.com>
2024-12-15 23:02:55 -0500

    Iron and copper automining finished

--- script.js
+++ script.js
@@ -1,30 +1,64 @@
-let ironCount = 0;
-let copperCount = 0;
+let ironOreCount = 0;
+let copperOreCount = 0;
 let ironDrillCount = 0;
-let autoMineToggle = false;
+let copperDrillCount = 0;
+
+// Ore mining rates in milliseconds. Default 1 per second.
+let ironOreMiningRate = 1000;
+let copperOreMiningRate = 1000;
+
+// Starting every counter on a different ID to prevent conflicts
+let ironIntervalId = 1;
+let copperIntervalId = 2;
 
 const ironButton = document.getElementById("ironOre");
 ironButton.onclick = function () {
-  ironCount++;
-  document.getElementById("ironCount").innerHTML = ironCount;
+  ironOreCount++;
+  document.getElementById("ironOreCount").innerHTML = ironOreCount;
 };
 
 const copperButton = document.getElementById("copperOre");
 copperButton.onclick = function () {
-  copperCount++;
-  document.getElementById("copperCount").innerHTML = copperCount;
+  copperOreCount++;
+  document.getElementById("copperOreCount").innerHTML = copperOreCount;
 };
 
 const ironDrillButton = document.getElementById("ironDrill");
 ironDrillButton.onclick = function () {
   ironDrillCount++;
   document.getElementById("ironDrillCount").innerHTML = ironDrillCount;
-  clearInterval();
-  setInterval(autoMineIron, ironDrillCount * 1000);
+  clearInterval(ironIntervalId);
+  recalculateIronOreMiningRate();
+  document.getElementById("ironOreMiningRate").innerHTML =
+    1000 / ironOreMiningRate + " per second";
+  ironIntervalId = setInterval(autoMineIron, ironOreMiningRate);
+};
+
+const copperDrillButton = document.getElementById("copperDrill");
+copperDrillButton.onclick = function () {
+  copperDrillCount++;
+  document.getElementById("copperDrillCount").innerHTML = copperDrillCount;
+  clearInterval(copperIntervalId);
+  recalculateCopperOreMiningRate();
+  document.getElementById("copperOreMiningRate").innerHTML =
+    1000 / copperOreMiningRate + " per second";
+  copperIntervalId = setInterval(autoMineCopper, copperOreMiningRate);
 };
 
 function autoMineIron() {
-  ironCount++;
-  document.getElementById("ironCount").innerHTML = ironCount;
-  console.log("Iron updated");
+  ironOreCount++;
+  document.getElementById("ironOreCount").innerHTML = ironOreCount;
+}
+
+function autoMineCopper() {
+  copperOreCount++;
+  document.getElementById("copperOreCount").innerHTML = copperOreCount;
+}
+
+function recalculateIronOreMiningRate() {
+  ironOreMiningRate = 1000 / ironDrillCount;
+}
+
+function recalculateCopperOreMiningRate() {
+  copperOreMiningRate = 1000 / copperDrillCount;
 }
